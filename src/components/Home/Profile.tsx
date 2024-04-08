f830dea816cbc6cf66c40356ee90235138f4f3f2
Takumi Yasufuku <run.taku12@gmail.com>
2024-04-08 19:03:47 +0900

    トップページの修正

--- src/components/Home/Profile.tsx
+++ src/components/Home/Profile.tsx
@@ -1,7 +1,5 @@
 import styles from "@/styles/Home/Profile.module.css";
 import Image from "next/image";
-import Link from "next/link";
-import { Headline } from "../Base/Headline";
 
 const imageStyle = {
   borderRadius: "5%",
@@ -11,11 +9,6 @@ const imageStyle = {
 export function Profile() {
   return (
     <div className={styles.wrapper}>
-      <Headline
-        title="Profile"
-        releaseDay="2024/04/20"
-        lastUpdateDay="2024/04/20"
-      />
       <div className={styles.profile}>
         <div className={styles.icon}>
           <Image
@@ -28,14 +21,12 @@ export function Profile() {
         </div>
         <div className={styles.content}>
           <h3 className={styles.name}>suyasuyaTY</h3>
-          <div className={styles.introduction}>
-            東京工業大学情報通信系学部4年です。
-            <br />
-            研究とWebプログラミングを少し頑張ってます。
-          </div>
-          <div className={styles.links}>
-            <Link href="/about"> もっと知りたい -&gt;</Link>
-          </div>
+          <h4>所属</h4>
+          <p className={styles.introduction}>東京工業大学情報通信系 (21B)</p>
+          <h4>趣味</h4>
+          <p className={styles.introduction}>ボカロ / プロセカ / スプラ</p>
+          <h4>日替わりコーナー</h4>
+          <p className={styles.introduction}>やさしいインターネット</p>
         </div>
       </div>
     </div>
