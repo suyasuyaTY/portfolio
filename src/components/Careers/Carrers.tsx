95333c4c57d1968203440f2e52995d543874eaa8
Takumi Yasufuku <run.taku12@gmail.com>
2024-04-08 19:00:49 +0900

    Careerの作成

---
+++ src/components/Careers/Carrers.tsx
@@ -0,0 +1,18 @@
+import { careers } from "@/consts/careers";
+import { Card } from "./Card";
+
+export function Careers() {
+  return (
+    <div style={{ width: "100%" }}>
+      {careers.map((carrer, index) => (
+        <Card
+          key={index}
+          title={carrer.title}
+          startDate={carrer.startDate}
+          endDate={carrer.endDate}
+          note={carrer.note}
+        />
+      ))}
+    </div>
+  );
+}
