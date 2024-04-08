7a5c89ed040d497d673176a220d2cf0e6bb93c1d
Takumi Yasufuku <run.taku12@gmail.com>
2024-04-08 18:59:07 +0900

    Worksの名前変更

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,16 +1,10 @@
 import { works } from "@/consts/works";
 import styles from "@/styles/Work/Work.module.css";
 import { Card } from "@/components/Works/Card";
-import { Headline } from "../Base/Headline";
 
-export function Work() {
+export function Works() {
   return (
     <div className={styles.wrapper}>
-      <Headline
-        title="Work"
-        releaseDay="2024/04/20"
-        lastUpdateDay="2024/04/20"
-      />
       {works.map((work, index) => (
         <Card
           key={index}
