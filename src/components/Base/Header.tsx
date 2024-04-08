a9fbc55d774ca3cb081ab48f93a75d2d2b57b1c0
Takumi Yasufuku <run.taku12@gmail.com>
2024-04-08 22:45:18 +0900

    ♻️Header

--- src/components/Base/Header.tsx
+++ src/components/Base/Header.tsx
@@ -1,27 +1,30 @@
 import Link from 'next/link';
-import styles from '@/styles/Base/Header.module.css';
+
+const links = [
+  { name: 'TOP', href: '/' },
+  { name: 'Works', href: '/works' },
+  { name: 'Articles', href: '/Articles' },
+];
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>
-        <Link href="/">TYの部屋</Link>
-      </h1>
-      <nav>
-        <ul className={styles.nav}>
-          <li>
-            <Link href="/">TOP</Link>
-          </li>
-          <li>
-            <Link href="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link href="/works">Works</Link>
-          </li>
-          <li>
-            <Link href="/articles">Articles</Link>
-          </li>
-        </ul>
+    <header className="fixed top-0 left-0 w-full h-12 border-b-2 border-gray-200">
+      <nav className="flex flex-row justify-between h-12">
+        <h1 className="w-48 text-xl ml-4 h-full flex items-center">
+          <Link href={'/'}>suyasuyaTY</Link>
+        </h1>
+        <div className="flex w-1/2">
+          {links.map((item, index) => (
+            <div
+              key={index}
+              className="flex-1 text-center align-middle hover:bg-slate-200 hover:border-b-2 hover:border-cyan-200 relative"
+            >
+              <Link href={item.href} className="flex w-full h-full items-center justify-center">
+                {item.name}
+              </Link>
+            </div>
+          ))}
+        </div>
       </nav>
     </header>
   );
