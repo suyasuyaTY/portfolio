88317add2d3feed179f44ed7fae90bb99bf81f3a
Takumi Yasufuku <run.taku12@gmail.com>
2024-04-10 16:37:33 +0900

    paddingの設定

--- src/components/pages/Top/Profile.tsx
+++ src/components/pages/Top/Profile.tsx
@@ -7,7 +7,7 @@ const imageStyle = {
 
 export function Profile() {
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-4xl mx-auto pb-16">
       <div className="flex justify-between mb-4  h-72">
         <div className="flex items-center">
           <Image src="/myIcon.jpg" alt="myicon" width={256} height={256} style={imageStyle} />
