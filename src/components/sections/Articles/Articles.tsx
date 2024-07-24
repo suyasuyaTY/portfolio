55cc5f734cc3d9efb4a52f61a6d2a080566baa7a
Takumi Yasufuku <run.taku12@gmail.com>
2024-07-24 12:10:38 +0900

    記事をapiから取得するための準備

--- src/components/sections/Articles/Articles.tsx
+++ src/components/sections/Articles/Articles.tsx
@@ -1,4 +1,3 @@
-import { ArticleLink, articleNoteLinks } from '@/consts/note/article';
 import { Section, SectionContent, SectionTitle } from '@/components/base/Section';
 import { MdArticle } from 'react-icons/md';
 import { ArticleCard } from './ArticleCard';
@@ -13,18 +12,11 @@ export function Articles() {
           <span className="ml-2 text-3xl">Articles</span>
         </div>
       </SectionTitle>
-      <SectionContent>
-        <CardList>
-          {articleLinks().map(({ title, url, imageLink, published_at }) => (
-            <ArticleCard key={title} title={title} url={url} imageLink={imageLink} published_at={published_at} />
-          ))}
-        </CardList>
-      </SectionContent>
     </Section>
   );
 }
 
-function articleLinks(): ArticleLink[] {
+/*function articleLinks(): ArticleLink[] {
   const note = articleNoteLinks.map((link) => ({
     ...link,
     description: 'note',
@@ -32,3 +24,4 @@ function articleLinks(): ArticleLink[] {
 
   return [...note].sort((a, b) => (a.published_at === b.published_at ? 0 : a.published_at < b.published_at ? 1 : -1));
 }
+*/
